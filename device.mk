7f8245e4af02ea6b288f8a5035cf4e7408dae28f
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    jasmine_sprout: Remove packages at build time

--- device.mk
+++ device.mk
@@ -299,6 +299,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \

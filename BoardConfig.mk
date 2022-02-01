482767cc2c07090d8546404236fc94ac5ab89523
Michael Bestas <mikeioannina@gmail.com>
2022-02-01 15:18:02 +0530

    jasmine_sprout: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS Signed-off-by: muti605 <mustafa.sahin605@yandex.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,8 +151,8 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
